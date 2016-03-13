2de04f419955c2b6b054f7fa0b08bac0ab41f58f
0yaoyao <dobeender.mc@gmail.com>
2016-03-13 14:40:47 +0800

    Update main.js

--- main.js
+++ main.js
@@ -1,3 +1,3 @@
-function fillp('inf-words'){
-  document.getElementById('inf-words').innerHTML = "永遠的25歲~~~";
+function fillp(elementID){
+  document.getElementById(elementID).innerHTML = "永遠的25歲~~~";
 }
