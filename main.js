8d3ce15c4a967f7c7ac7f89d82f6fa92b61ba9d9
0yaoyao <dobeender.mc@gmail.com>
2016-03-13 14:48:54 +0800

    Update main.js

--- main.js
+++ main.js
@@ -1,3 +1,13 @@
+
+//functions
+function fillp(elementID){
+  document.getElementById(elementID).innerHTML = "永遠的25歲~~~";
+}
+
+function fillpr(elementID){
+  document.getElementById(elementID).innerHTML = "堯堯杯世界美女獎第一名","test";
+}
+
 function fillp(elementID){
   document.getElementById(elementID).innerHTML = "永遠的25歲~~~";
 }
