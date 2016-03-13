69b079a45fbaa49491127263e6298b8df7609434
0yaoyao <dobeender.mc@gmail.com>
2016-03-13 15:04:55 +0800

    Update main.js

--- main.js
+++ main.js
@@ -1,12 +1,10 @@
-
 //functions
 function fillp(elementID){
   document.getElementById(elementID).innerHTML = "永遠的25歲~~~";
 }
 
 function fillpr(elementID){
-  document.getElementById(elementID).innerHTML = "堯堯杯世界美女獎第一名111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111";
-}
+  document.getElementById(elementID).innerHTML = "堯堯杯世界美女獎第一名 11";
 
 function fillp(elementID){
   document.getElementById(elementID).innerHTML = "永遠的25歲~~~";
